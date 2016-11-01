5b0c3d9c006acbba7258368d58101fa4c37c683a
TaoistBear <originaltaoistbear@gmail.com>
2016-11-01 13:38:55 -0400

    initial commit
    base set up with IIFE and ‘use strict’

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
 //******************************************************************************
 //Tic-Tac-Toe P4-Treehouse Full Stack Tech Degree
 //******************************************************************************
+!function() {
+  'use strict';
+  //******************************************************************************
+  //on-load show start up screen, use tictactoe-01-start.png mockup and start.txt html snippet
+  //******************************************************************************
 
-//******************************************************************************
-//on-load show start up screen, use tictactoe-01-start.png mockup and start.txt html snippet
-//******************************************************************************
+  //******************************************************************************
+  //start button removes the start screen and begin the game with game board, use titctactoe-02-inprogress.png mockup and board.txt html snippet
+  //******************************************************************************
 
-//******************************************************************************
-//start button removes the start screen and begin the game with game board, use titctactoe-02-inprogress.png mockup and board.txt html snippet
-//******************************************************************************
+  //******************************************************************************
+  //add game play for the following rules
+  //  *play alternates between X and O
+  //  *current player indicated (hint add class .active)
+  //  *on hover the player symbol should appear on the square
+  //  *can only click on empty squares add class .box-filled-1 (for ) .box-filled-2 (for x)
+  //  *game ends when 3 in a row (horizontal, diagnoal, virticle) or draw
+  //******************************************************************************
 
-//******************************************************************************
-//add game play for the following rules
-//  *play alternates between X and O
-//  *current player indicated (hint add class .active)
-//  *on hover the player symbol should appear on the square
-//  *can only click on empty squares add class .box-filled-1 (for ) .box-filled-2 (for x)
-//  *game ends when 3 in a row (horizontal, diagnoal, virticle) or draw
-//******************************************************************************
+  //******************************************************************************
+  //game end: board dissapears with winner or draw screen use tictactoe-03-winner1.png and tictactoe-04-winner4.png mockups along with win.txt snippet for guidance
+  //******************************************************************************
 
-//******************************************************************************
-//game end: board dissapears with winner or draw screen use tictactoe-03-winner1.png and tictactoe-04-winner4.png mockups along with win.txt snippet for guidance
-//******************************************************************************
+  //******************************************************************************
+  //when player pushes new game the board resets
+  //******************************************************************************
 
-//******************************************************************************
-//when player pushes new game the board resets
-//******************************************************************************
+  //******************************************************************************
+  //use a module pattern to wrapped JS in IIF
+  //******************************************************************************
 
-//******************************************************************************
-//use a module pattern to wrapped JS in IIF
-//******************************************************************************
-
-//******************************************************************************
-//*** EXTRA CREDIT
-//*** *** ALLOW PLAYERS TO ENTER THEIR NAME AND WIN PAGE SHOULD SHOW NAME
-//******************************************************************************
+  //******************************************************************************
+  //*** EXTRA CREDIT
+  //*** *** ALLOW PLAYERS TO ENTER THEIR NAME AND WIN PAGE SHOULD SHOW NAME
+  //******************************************************************************
+}();
