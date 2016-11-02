f8847879fd48076664f84f472141c247553f8693
TaoistBear <originaltaoistbear@gmail.com>
2016-11-02 10:27:46 -0400

    commit for win check almost done
    variables for checks follow through.  need to examine the console.log
    in order to find out why it is every-other one and not first then
    second.  this is my error.  almost there.

--- js/script.js
+++ js/script.js
@@ -26,12 +26,24 @@
 
   //******************************************************************************
   //add game play for the following rules
-  //  *play alternates between X and O
-  //  *current player indicated (hint add class .active)
+  //  *** - DONE - *** *play alternates between X and O
+  //  *** - DONE - *** *current player indicated (hint add class .active)
   //  *on hover the player symbol should appear on the square
-  //  *can only click on empty squares add class .box-filled-1 (for ) .box-filled-2 (for x)
+  //  *** - DONE - *** *can only click on empty squares add class .box-filled-1 (for ) .box-filled-2 (for x)
   //  *game ends when 3 in a row (horizontal, diagnoal, virticle) or draw
   //******************************************************************************
+
+  //SET UP ARRAYS IN OBJECT TO HOLD WINNING COMBINATIONS
+  var winCombo = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+  //SET UP COUNTER TO COUNT ALL MOVES IF REACHES 9 BEFORE A WIN THEN GAME IS DRAW
+  var counter = 0;
+  //KEEPOING PLAYERSELECT VARIABLES GLOBAL IN ORDER FOR ACCESS AND CHECKING
+  var player1Select = [];
+  var player2Select = [];
+
+  //CREATE HELPER FUNCTION TO CHECK FOR A WIN!
+
+
   //START OF GAME PLAY FUNCTION
   $('.box').click(function() {
     //SET THE PLAYER SELECTION ON CLICK WITH PREVIOUSLY DEFINED TOGGLE
@@ -40,16 +52,34 @@
         return;
     } else if ($('#player1').hasClass('active')) {
       $(this).addClass('box-filled-1');
+      player1Select.push($(this).index());
+      //SET UP FOR LOOP TO CHECK FOR WINNING COMBOS WITH PLAYER SELECT ARRAY
+      for (var i = 0; i < 8; i++) {
+        var comoboCheck = winCombo[i];
+        for (var s = 0; s < player1Select.length; s++) {
+          var hitCount = 0;
+          var hitCheck = player1Select[s];
+          console.log(comoboCheck);
+          console.log(hitCheck);
+          // if (comoboCheck.indexOf(player1Select) >= 0) {
+          //   console.log("hit");
+          // }
+        }
+      }
     } else {
       $(this).addClass('box-filled-2');
+      player2Select.push($(this).index());
     }
+
     //TOGGLE 'ACTIVE' CLASS TO DETERMINE WHO IS PLAYING
     $('.players').toggleClass('active');
+    counter++;
   })
   //SET UP HOVER LISTENER TO CHANGE BACKGROUND TO SHOW PLAYER SYMBOL
   $('.box').mouseover(function() {
     var $hoverBox = $(this);
   })
+
   //******************************************************************************
   //game end: board dissapears with winner or draw screen use tictactoe-03-winner1.png and tictactoe-04-winner4.png mockups along with win.txt snippet for guidance
   //******************************************************************************
